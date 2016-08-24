84e7b85245166e5e39108c89a248cea7159053f5
Nhat Ho <nhatminhhoca@gmail.com>
2016-08-23 23:21:13 -0400

    Trying to commit all files again manually

--- dist/components/view/Header.js
+++ dist/components/view/Header.js
@@ -12,16 +12,7 @@ var Header = (function (_super) {
         _super.apply(this, arguments);
     }
     Header.prototype.render = function () {
-        return React.createElement("header", null, React.createElement("div", {className: "container-fluid"}, React.createElement("div", {className: "header-content"}, React.createElement("div", {className: "header-name"}, "Flexible Tour Framework"), React.createElement("div", {className: "header-subname"}, "Guide your customers through your amazing application."), React.createElement("a", {
-            className: "page-scroll btn btn-success btn-lg",
-            onClick: this.onClick
-        }, React.createElement("span", {
-            className: "glyphicon glyphicon-play-circle",
-            "aria-hidden": "true"
-        }), React.createElement("span", {className: "header-button"}, "Demo !")), React.createElement("div", {className: "media-block"}, React.createElement("a", {
-            href: "https://github.com/NhatHo/FlexTourJs",
-            target: "_blank"
-        }, React.createElement("span", {className: "fa fa-github fa-4x"}))))));
+        return React.createElement("header", null, React.createElement("div", {className: "container-fluid"}, React.createElement("div", {className: "header-content"}, React.createElement("div", {className: "header-name"}, "Flexible Tour Framework"), React.createElement("div", {className: "header-subname"}, "Guide your customers through your amazing application."), React.createElement("a", {className: "page-scroll btn btn-success btn-lg", onClick: this.onClick}, React.createElement("span", {className: "glyphicon glyphicon-play-circle", "aria-hidden": "true"}), React.createElement("span", {className: "header-button"}, "Demo !")), React.createElement("div", {className: "media-block"}, React.createElement("a", {href: "https://github.com/NhatHo/FlexTourJs", target: "_blank"}, React.createElement("span", {className: "fa fa-github fa-4x"}))))));
     };
     Header.prototype.onClick = function (event) {
         event.stopPropagation();
@@ -29,21 +20,21 @@ var Header = (function (_super) {
         var tourDesc = {
             id: "fontpageTour",
             steps: [{
-                content: "This is the new features section",
-                title: "New Features",
-                position: "top",
-                target: "#desc"
-            }, {
-                content: "You can checkout the demo codes in this section so that you can apply them in your own apps.",
-                title: "Demo",
-                position: "top",
-                target: "#attributes"
-            }, {
-                content: "The API of this framework is explained in details here. This will help you greatly in writing your own tours.",
-                title: "API",
-                position: "top",
-                target: "#wiki"
-            }]
+                    content: "This is the new features section",
+                    title: "New Features",
+                    position: "top",
+                    target: "#desc"
+                }, {
+                    content: "You can checkout the demo codes in this section so that you can apply them in your own apps.",
+                    title: "Demo",
+                    position: "top",
+                    target: "#attributes"
+                }, {
+                    content: "The API of this framework is explained in details here. This will help you greatly in writing your own tours.",
+                    title: "API",
+                    position: "top",
+                    target: "#wiki"
+                }]
         };
         var tourDescs = [tourDesc];
         var flextour = new FlexTour(tourDescs, {});
--- dist/components/view/sections/Description.js
+++ dist/components/view/sections/Description.js
@@ -13,40 +13,7 @@ var Description = (function (_super) {
         _super.apply(this, arguments);
     }
     Description.prototype.render = function () {
-        return (React.createElement("section", {id: "desc"}, React.createElement(SectionHeader_1.SectionHeader, {
-            header: "What's New?",
-            subHeader: "The developers now can fully control the flow of the tour to compensate for application's performance."
-        }), React.createElement("div", {className: "container"}, React.createElement("div", {className: "row text-center"}, React.createElement(NewFeatures_1.NewFeatures, {
-            title: "Shall you pass?",
-            content: "You can check if the next step should be showed or not. FlexTourJS will not show the next step until all prerequisites are met.",
-            icon: "fa-check-square",
-            code: "prerequisites: ['condition1','condition2', ...]"
-        }), React.createElement(NewFeatures_1.NewFeatures, {
-            title: "Are you ready?",
-            content: "Processing might take some time, FlexTourJs ensures that the next step will be displayed after your application has completed rendering.",
-            icon: "fa-clock-o",
-            code: "prerequisites: ['?isVisible:params', ...]"
-        }), React.createElement(NewFeatures_1.NewFeatures, {
-            title: "Uh oh!",
-            content: "When the target of your next step does not show or it is conditional, you can set up a back up route and FlexTourJS will flow to it.",
-            icon: "fa-meh-o",
-            code: "skip: {branch-step-number}, prerequisites: ['!proceedCondition:params', ...]"
-        })), React.createElement("div", {className: "row text-center"}, React.createElement(NewFeatures_1.NewFeatures, {
-            title: "Power!!!",
-            content: "The power is in your hands, you can create new bubble styles, add new buttons and attach functionalities to them. Let your imagination run wild.",
-            icon: "fa-unlock",
-            code: "buttons:[{name: '', style: '', onclick: func()}, ...]"
-        }), React.createElement(NewFeatures_1.NewFeatures, {
-            title: "Show off features?",
-            content: "FlexTourJS will not limit your application in any way, you can do drag and drop, popup menu, dropdown menu, etc. without breaking your flow.",
-            icon: "fa-arrows",
-            code: "dragAndDrop:true, modal:true, ..."
-        }), React.createElement(NewFeatures_1.NewFeatures, {
-            title: "Everyone can use it!",
-            content: "Yes, everyone can use it because FlexTourJS comes with NLS ready as well as fully accessible using keyboard.",
-            icon: "fa-universal-access",
-            code: "nextButton:{newName}, backButton:{newName}, skipButton:{newName}, doneButton:{newName}, ..."
-        })))));
+        return (React.createElement("section", {id: "desc"}, React.createElement(SectionHeader_1.SectionHeader, {header: "What's New?", subHeader: "The developers now can fully control the flow of the tour to compensate for application's performance."}), React.createElement("div", {className: "container"}, React.createElement("div", {className: "row text-center"}, React.createElement(NewFeatures_1.NewFeatures, {title: "Shall you pass?", content: "You can check if the next step should be showed or not. FlexTourJS will not show the next step until all prerequisites are met.", icon: "fa-check-square", code: "prerequisites: ['condition1','condition2', ...]"}), React.createElement(NewFeatures_1.NewFeatures, {title: "Are you ready?", content: "Processing might take some time, FlexTourJs ensures that the next step will be displayed after your application has completed rendering.", icon: "fa-clock-o", code: "prerequisites: ['?isVisible:params', ...]"}), React.createElement(NewFeatures_1.NewFeatures, {title: "Uh oh!", content: "When the target of your next step does not show or it is conditional, you can set up a back up route and FlexTourJS will flow to it.", icon: "fa-meh-o", code: "skip: {branch-step-number}, prerequisites: ['!proceedCondition:params', ...]"})), React.createElement("div", {className: "row text-center"}, React.createElement(NewFeatures_1.NewFeatures, {title: "Power!!!", content: "The power is in your hands, you can create new bubble styles, add new buttons and attach functionalities to them. Let your imagination run wild.", icon: "fa-unlock", code: "buttons:[{name: '', style: '', onclick: func()}, ...]"}), React.createElement(NewFeatures_1.NewFeatures, {title: "Show off features?", content: "FlexTourJS will not limit your application in any way, you can do drag and drop, popup menu, dropdown menu, etc. without breaking your flow.", icon: "fa-arrows", code: "dragAndDrop:true, modal:true, ..."}), React.createElement(NewFeatures_1.NewFeatures, {title: "Everyone can use it!", content: "Yes, everyone can use it because FlexTourJS comes with NLS ready as well as fully accessible using keyboard.", icon: "fa-universal-access", code: "nextButton:{newName}, backButton:{newName}, skipButton:{newName}, doneButton:{newName}, ..."})))));
     };
     return Description;
 }(React.Component));
